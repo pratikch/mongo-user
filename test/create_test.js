397d91a6a896812e06a39b93ee142b339e183d31
Pratik Chaudhari <cp.baba@gmail.com>
2018-08-25 18:17:38 -0700

    create a record e2e

--- test/create_test.js
+++ test/create_test.js
@@ -1,10 +1,18 @@
 const mongoose = require('mongoose');
+const assert = require('assert');
+const User = require('../src/user');
 
 describe('Create user records', () => {
     // it('', () => {
 
     // });
-    it('saves a user', () => {
-        
+    it('saves a user', (done) => {
+        const joe = new User({ name: 'Pratik', lastName: 'Chaudhari' });
+        joe.save()
+            .then(() => {
+                assert(!joe.isNew);
+                done();
+            });
+
     });
 });
--- test/test_helper.js
+++ test/test_helper.js
@@ -1,10 +1,20 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/users_test', {useNewUrlParser: true});
-mongoose.connection
-    .once('open', () => {
-        console.log('Good to go!');
-    })
-    .on('error', (error) => { 
-        console.warn('Warning', error);
+before((done) => {
+    mongoose.connect('mongodb://localhost/users_test', { useNewUrlParser: true });
+    mongoose.connection
+        .once('open', () => {
+            console.log('Good to go!');
+            done();
+        })
+        .on('error', (error) => {
+            console.warn('Warning', error);
+        });
+
+});
+
+beforeEach((done) => {
+    mongoose.connection.collections.users.drop(() => {
+        done();
     });
+});
